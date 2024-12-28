cd649eaa2cb17dfe57d2a20c8c78fa02dc6d1ebd
farid118 <farid.masjedi1985@gmail.com>
2024-12-28 22:36:53 +1100

    add quicker method | prime

--- orchestration/prime.mjs
+++ orchestration/prime.mjs
@@ -1,5 +1,6 @@
 import {
   checkAndExplainPrimeStatus, // *Main 6 (much quicker | will check the existing files, and if number is smaller than greatest foldernumber, it will find it, otherwise will use isPrime, which is slow way)
+  checkAndExplainPrimeStatusUpdated, // *Main 6-1 (much quicker | quicker than checkAndExplainPrimeStatus)
 } from "../services/helper.mjs";
 import {
   calculateDivisors, // Main 2 ( take too much time | This will check divisors from first divisor which is on prime pattern )
@@ -8,6 +9,7 @@ import {
 import {
   isPrime, // Main 1 ( take too much time | This will check divisors from first divisor which is on prime pattern)
   isPrimeUsingFiles, // *Main 5 (much quicker | will check the existing files, and if number is smaller than greatest foldernumber, it will find it, otherwise will use isPrime, which is slow way)
+  isPrimeUsingFilesUpdated, // *Main 5-1 (much quicker | quicker than isPrimeUsingFiles )
   isPrimeFromText, // **Main 9 | This is super quick | it will check:
   // - if sqrt less than existing numbers on folders , it will check if the number is dividable by any prime less than that
   // - if no, it will find the last existing and check if the number is dividable by any prime there
@@ -17,25 +19,31 @@ import {
   // - if no, it will find the last existing and check if the number is dividable by any prime there
   // - if no, it will use checkDivisorNotExistOnTextFiles, will check from number lastNumber on lastfolder + 1 to the sqrtNumber, to find primes and check if the number is dividable by any of them
   // This will not create new folders.
+  isPrimeFromTextFilesUpdated, // **Main 10-1 | This is quicker than isPrimeFromTextFiles
   isPrimeFromTextFilesRecursive, // ***Main 11 | This is super quick | it will check:
   // - if sqrt less than existing numbers on folders , it will check if the number is dividable by any prime less than that
   // - if no, it will find the last existing and check if the number is dividable by any prime there
   // - if no, it will use checkDivisorNotExistOnTextFilesRecursive, which is quicker than checkDivisorNotExistOnTextFiles
   // This will not create new folders.
+  isPrimeFromTextFilesRecursiveUpdated, // ***Main 11-1 | This is quicker than isPrimeFromTextFilesRecursive
   isPrimeFromTextRecursive, // **Main 12 | This is super quick | it will check:
   // - if sqrt less than existing numbers on folders , it will check if the number is dividable by any prime less than that
   // - if no, it will find the last existing and check if the number is dividable by any prime there
   // - if no, it will use formatLastFileInLastFolderRecursive, which is quicker than formatLastFileInLastFolder, as it use generatePrimesUpToRecursive which will use isPrimeFromTextFilesRecursive, which is super quick one.
+  isPrimeFromTextRecursiveUpdated, // **Main 12-1 | This is quicker than isPrimeFromTextRecursive
 } from "../services/primeChecker.mjs";
 
 import {
   generatePrimesFiles, // ***Main 14 | Super quick
+  generatePrimesFilesUpdated, // ***Main 14-1 | quicker than generatePrimesFiles
   generatePrimesUpToRecursive, // ***Main 13 | Super quick
-  generatePrimesUpToRecursiveUpdated, // ***Main 13 | Super quick | quicker than generatePrimesUpToRecursive
+  generatePrimesUpToRecursiveUpdated, // ***Main 13-1 | Super quick | quicker than generatePrimesUpToRecursive
+  generatePrimesRecursiveUpdated, // ***Main 13-2 | Super quick | quicker than generatePrimesUpToRecursiveUpdated
   generatePrimeOutputFromText, // **Main 7 ( super quick | this has a limitation which will just create the output, if the number we input is less than the greatest one that already exists)
   generatePrimesInRange, // Main 4 ( take too much time | quicker than previous ones | This will check divisors from first divisor which is on prime pattern )
   generatePrimesInRangeUpdated, // Main 4-1 ( take too much time | quicker than previous ones | This will check divisors from first divisor which is on prime pattern | Quicker than generatePrimesInRange)
   generatePrimesInRangeTextFiles, // Main 4-2 | Super Quick
+  generatePrimesInRangeTextFilesUpdated, // Main 4-3 | quicker than generatePrimesInRangeTextFiles
   generatePrimesUpTo, // Main 3 ( take too much time | This will create primes from first prime)
   generatePrimesUpToUpdated, // Main 3-1 ( take too much time | This will create primes from first prime | quicker than generatePrimesUpTo)
 } from "../services/primeGenerator.mjs";
