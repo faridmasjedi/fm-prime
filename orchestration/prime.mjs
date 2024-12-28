5d17c76a3cfcb8ae2cd5b42927dbbc312fb4eae7
farid118 <farid.masjedi1985@gmail.com>
2024-12-28 18:21:58 +1100

    Update prime.mjs

--- orchestration/prime.mjs
+++ orchestration/prime.mjs
@@ -1,4 +1,4 @@
-import { 
+import {
   checkAndExplainPrimeStatus, // *Main 6 (much quicker | will check the existing files, and if number is smaller than greatest foldernumber, it will find it, otherwise will use isPrime, which is slow way)
 } from "../services/helper.mjs";
 import {
@@ -9,10 +9,9 @@ import {
   isPrime, // Main 1 ( take too much time | This will check divisors from first divisor which is on prime pattern)
   isPrimeUsingFiles, // *Main 5 (much quicker | will check the existing files, and if number is smaller than greatest foldernumber, it will find it, otherwise will use isPrime, which is slow way)
   isPrimeFromText, // **Main 9 | This is super quick | it will check:
-                               // - if sqrt less than existing numbers on folders , it will check if the number is dividable by any prime less than that
-                               // - if no, it will find the last existing and check if the number is dividable by any prime there
-                               // - if no, it will create a folder for sqrt number, by coping all the existing ones and then using generatePrimesUpTo to create the rest of the prime, from last existing prime number. Then it will check if it is dividable by any prime there or not
-
+  // - if sqrt less than existing numbers on folders , it will check if the number is dividable by any prime less than that
+  // - if no, it will find the last existing and check if the number is dividable by any prime there
+  // - if no, it will create a folder for sqrt number, by coping all the existing ones and then using generatePrimesUpTo to create the rest of the prime, from last existing prime number. Then it will check if it is dividable by any prime there or not
   isPrimeFromTextFiles, // **Main 10 | This is super quick | it will check:
   // - if sqrt less than existing numbers on folders , it will check if the number is dividable by any prime less than that
   // - if no, it will find the last existing and check if the number is dividable by any prime there
@@ -26,13 +25,16 @@ import {
   isPrimeFromTextRecursive, // **Main 12 | This is super quick | it will check:
   // - if sqrt less than existing numbers on folders , it will check if the number is dividable by any prime less than that
   // - if no, it will find the last existing and check if the number is dividable by any prime there
-  // - if no, it will use formatLastFileInLastFolderRecursive, which is quicker than formatLastFileInLastFolder, as it use generatePrimesUpToRecursive which will use isPrimeFromTextFilesRecursive, which is super quick one. 
+  // - if no, it will use formatLastFileInLastFolderRecursive, which is quicker than formatLastFileInLastFolder, as it use generatePrimesUpToRecursive which will use isPrimeFromTextFilesRecursive, which is super quick one.
 } from "../services/primeChecker.mjs";
 
 import {
-  generatePrimesFiles, // ***Main 14 | Super quick 
+  generatePrimesFiles, // ***Main 14 | Super quick
   generatePrimesUpToRecursive, // ***Main 13 | Super quick
+  generatePrimesUpToRecursiveUpdated, // ***Main 13 | Super quick | quicker than generatePrimesUpToRecursive
   generatePrimeOutputFromText, // **Main 7 ( super quick | this has a limitation which will just create the output, if the number we input is less than the greatest one that already exists)
   generatePrimesInRange, // Main 4 ( take too much time | quicker than previous ones | This will check divisors from first divisor which is on prime pattern )
+  generatePrimesInRangeUpdated, // Main 4-1 ( take too much time | quicker than previous ones | This will check divisors from first divisor which is on prime pattern | Quicker than generatePrimesInRange)
   generatePrimesUpTo, // Main 3 ( take too much time | This will create primes from first prime)
+  generatePrimesUpToUpdated, // Main 3-1 ( take too much time | This will create primes from first prime | quicker than generatePrimesUpTo)
 } from "../services/primeGenerator.mjs";
